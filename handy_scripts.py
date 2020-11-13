617d7b0ee4c933ba4f0510118da0204fb3a6c2bb
thissop <68932815+thissop@users.noreply.github.com>
2020-11-13 09:11:12 -0500

    Update handy_scripts.py

--- handy_scripts.py
+++ handy_scripts.py
@@ -3,6 +3,10 @@
 ##between points (see distance parameter in the scipy.signal.find_peaks documentation)
 ##Reccomend that you set an array equal to the result of this, e.g. peaks = calculate_peaks(data_aray=,widthval=)
 def calculate_peaks(data_array,widthval):
+    #Imports
+    from scipy.signal import find_peaks
+    import numpy as np
+    #Action
     fakepeaks, _ = find_peaks(data_array,width=widthval)
     realpeaks, _ = find_peaks((data_array*-1),width=widthval)
     outarray = np.concatenate((fakepeaks,realpeaks))
