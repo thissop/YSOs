e047bf9d5f5fd7ec53c38a470c286854c0692cf1
thissop <68932815+thissop@users.noreply.github.com>
2020-12-03 08:26:21 -0500

    Add files via upload

---
+++ ysospy/variability.py
@@ -0,0 +1,18 @@
+def sergisonDistribution(x,percentiles):
+    #Import(s)
+    import numpy as np
+    
+    #Action
+    mag_list = list(x)
+    mean_mag = np.mean(np.array(mag_list))
+    lower_percentile,upper_percentile = np.percentile(np.array(mag_list),[percentiles[0], percentiles[1]])
+    amp_metric = lower_percentile-upper_percentile
+    
+
+    normalized_mags = []
+
+    for item in mag_list:
+        normalized_mag = (item-mean_mag)/amp_metric
+        normalized_mags.append(normalized_mag)
+
+    return amp_metric, np.array(normalized_mags) # Return results
