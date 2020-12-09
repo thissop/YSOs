072bf009b2390442f98732229d6e3665b54c661a
thissop <68932815+thissop@users.noreply.github.com>
2020-12-09 09:37:55 -0500

    Update variability.py

--- ysospy/variability.py
+++ ysospy/variability.py
@@ -1,3 +1,30 @@
+def calculateFluxAsymmetry(x):
+    #Description: Returns the flux asymmetry metric M from Bredall et al. 2019
+    
+    #Import(s)
+    import numpy as np
+    from scipy import stats
+
+    #Action
+    x = list(x)
+    
+    bottom10 = [] #bottom 10% of magnitude measurements
+    top10 = [] #top 10% of magnitude measurements
+
+    for item in x:
+        if stats.percentileofscore(x,item) <= 10:
+            bottom10.append(item)
+        elif stats.percentileofscore(x,item) >= 90:
+            top10.append(item)
+    
+    topandbottom10 = np.array(bottom10+top10)
+    m10 = np.mean(topandbottom10)
+    x_median = np.median(x) 
+    sd = np.std(x)
+    M = (m10-x_median)/(sd) #Flux Asymmetry metric, M
+    
+    return M
+
 def sergisonDistribution(x,percentiles):
     #Import(s)
     import numpy as np
